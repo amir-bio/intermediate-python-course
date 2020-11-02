4a8cbdbf01cca98dc55decb2e4386a1026d32a5e
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2020-11-02 09:23:01 +0000

    Add custom messages

--- dice_roller.py
+++ dice_roller.py
@@ -3,10 +3,16 @@ import random
 def main():
   dice_rolls = 2
   s = 0
+
   for i in range(0, dice_rolls):
     roll = random.randint(1,6)
     s += roll
-    print(f'You rolled {roll}')
+    if roll == 1:
+      print( 'You rolled a 1! Critical Fail :(')
+    elif roll == 6:
+      print('You rolled a 6! Critical Success! :)')
+    else:
+      print(f'You rolled a {roll}')
   print(f'Total rolls: {s}')
 
 if __name__== "__main__":
