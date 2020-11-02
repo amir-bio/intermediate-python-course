2a9b7aae14520e9e45263255524f67b44fa5a927
Amirhossein Andohkosh <4315615+amirha97@users.noreply.github.com>
2020-11-02 09:09:42 +0000

    Add 2 dice rolls

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+import random
+
 def main():
-  print('You rolled a die')
+  dice_rolls = 2
+  s = 0
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    s += roll
+    print(f'You rolled {roll}')
+  print(f'Total rolls: {s}')
 
 if __name__== "__main__":
   main()
